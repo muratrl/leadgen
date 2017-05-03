e6e36142cfc8e4d39a5118c7806abe90e5351ade
muratrl <murat@rentlever.com>
2017-05-03 10:01:18 -0700

    adding sorting

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,16 @@
 class DashboardController < ApplicationController
-
+  helper_method :sort_column, :sort_direction
   def index
-    @prospects=Prospect.joins("RIGHT OUTER JOIN prospect_linkedin on prospect_linkedin.prospect_user_id=prospect_user.id ").paginate(:page => params[:page], :per_page => 30)
+    @prospects=Prospect.
+    joins("RIGHT OUTER JOIN prospect_linkedin on prospect_linkedin.prospect_user_id=prospect_user.id ").
+    paginate(:page => params[:page], :per_page => 30).order(sort_column + ' ' + sort_direction)
   end
+  private
+   def sort_column
+     params[:sort] || "owner_1_first_name"
+   end
+
+   def sort_direction
+     params[:direction] || "asc"
+   end
 end
