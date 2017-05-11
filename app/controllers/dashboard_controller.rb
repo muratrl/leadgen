5b545e90f1c403d3f95dcca45208b1477d05a958
mgocmen <murat@rentlever.com>
2017-05-11 12:55:37 -0700

    add zestimate and show

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,8 +2,8 @@ class DashboardController < ApplicationController
   helper_method :sort_column, :sort_direction
   before_action :authenticate_user!
   def index
-    @prospects=Prospect.
-    joins("RIGHT OUTER JOIN prospect_linkedin on prospect_linkedin.prospect_user_id=prospect_user.id ").
+    @prospects=Prospect.joins("RIGHT   JOIN prospect_linkedin on prospect_linkedin.prospect_user_id=prospect_user.id  RIGHT JOIN prospect_rental on prospect_rental.prospect_user_id=prospect_user.id ").
+    select("prospect_user.*,prospect_linkedin.*,prospect_rental.*").
     search(params[:search]).order(sort_column + ' ' + sort_direction).
     paginate(:page => params[:page], :per_page => 150)
   end
@@ -16,3 +16,5 @@ class DashboardController < ApplicationController
      params[:direction] || "asc"
    end
 end
+
+Linkedin.joins("LEFT JOIN prospect_user pu  on prospect_user.id=prospect_user.prospect_user_id")
